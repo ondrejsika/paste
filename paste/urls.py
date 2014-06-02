a4da84c757fdeead4969ea7d5b2c415afbfcd630
Ondrej Sika <ondrej@ondrejsika.com>
2014-06-02 20:37:01 +0200

    fix urls for django 1.6

--- paste/urls.py
+++ paste/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls import patterns, include, url
 from django.views.generic.base import TemplateView
 
 from django.contrib.auth.views import login as login_view, logout as logout_view
